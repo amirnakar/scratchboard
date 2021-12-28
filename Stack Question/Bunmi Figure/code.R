43069f8782d0e23548bb47821dd4b7ede57ca6d1
amirnakar <anakar@ymail.com>
2021-12-28 21:02:41 +0100

    progress

--- Stack Question/Bunmi Figure/code.R
+++ Stack Question/Bunmi Figure/code.R
@@ -10,7 +10,36 @@ library(tidyverse)
 data$Conc.Cat = as.character(data$Conc)
 colnames(data)
 data %>% 
-  ggplot(aes(x=Class,y=DS)) +
-  geom_boxplot(aes(fill = Conc)) +
-  geom_jitter(aes(fill = Conc)) 
+  ggplot(aes(
+    x=Class,y=DS, col=Conc.Cat, fill = Conc.Cat)) +
+  scale_fill_brewer(palette="Greens") + 
+  scale_color_brewer(palette="Greens") + 
+  #theme(panel.background = "black") + 
+  stat_summary(
+    fun = mean, fun.min = mean, fun.max = mean, 
+    geom = "crossbar", 
+    size = 0.3) + 
+  geom_dotplot(,
+               binaxis = "y",         # which axis to bin along
+               binwidth = 0.05,       # Minimal difference considered diffeerent
+               stackdir = "center",   # Centered
+               dotsize = 0.5
+  )
+
+
  # geom_dotplot(aes(x=Class,y=DS), method="histodot",stackgroups=TRUE, binwidth = 1)
+
+library(RColorBrewer)
+display.brewer.all()
+
+
+
+p= data %>% 
+  ggplot(aes(x=Class,y=DS)) +
+  geom_point(size=3) +
+  stat_summary(
+    fun.y = mean, fun.ymin = mean, fun.ymax = mean, 
+    geom = "crossbar", 
+    color = "blue",
+    size = 0.3)
+
