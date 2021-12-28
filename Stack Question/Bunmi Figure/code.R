1b9a1adedb4e86db350cc22e3c6c97622b7a1d5f
Amir Nakar <di58lag@ads.uni-jena.de>
2021-12-28 16:39:21 +0100

    upload

---
+++ Stack Question/Bunmi Figure/code.R
@@ -0,0 +1,16 @@
+library(readxl)
+pred_DA_svm <- read_excel("Stack Question/Bunmi Figure/pred DA svm.xlsx", sheet = "Sheet3")
+View(pred_DA_svm)
+
+head(pred_DA_svm)
+data = pred_DA_svm
+
+library(tidyverse)
+
+data$Conc.Cat = as.character(data$Conc)
+
+data %>% 
+  ggplot(aes(x=Class,y=DS))+
+  geom_boxplot(aes(fill = Conc.Cat, fill))+
+  geom_jitter(aes(fill = Conc.Cat)) 
+ # geom_dotplot(aes(x=Class,y=DS), method="histodot",stackgroups=TRUE, binwidth = 1)
