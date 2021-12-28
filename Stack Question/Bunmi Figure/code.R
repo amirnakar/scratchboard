a20422ca583224488c847a02d1d502ec415b1427
amirnakar <anakar@ymail.com>
2021-12-28 21:59:38 +0100

    Finalized

--- Stack Question/Bunmi Figure/code.R
+++ Stack Question/Bunmi Figure/code.R
@@ -1,45 +1,43 @@
+### Load Libraries
 library(readxl)
-pred_DA_svm <- read_excel("Stack Question/Bunmi Figure/pred DA svm.xlsx", sheet = "Sheet3")
-View(pred_DA_svm)
+library(tidyverse)
+library(RColorBrewer)
 
+### Read Data
+pred_DA_svm <- read_excel("Stack Question/Bunmi Figure/pred DA svm.xlsx", sheet = "Sheet3")
 head(pred_DA_svm)
 data = pred_DA_svm
 
-library(tidyverse)
-
 data$Conc.Cat = as.character(data$Conc)
 colnames(data)
-data %>% 
-  ggplot(aes(
-    x=Class,y=DS, col=Conc.Cat, fill = Conc.Cat)) +
-  scale_fill_brewer(palette="Greens") + 
-  scale_color_brewer(palette="Greens") + 
-  #theme(panel.background = "black") + 
-  stat_summary(
-    fun = mean, fun.min = mean, fun.max = mean, 
-    geom = "crossbar", 
-    size = 0.3) + 
-  geom_dotplot(,
+
+### Plot Data
+
+plot = data %>% 
+  ggplot(aes(                             # Part 1: what are the axis
+    x=Class,
+    y=DS, 
+    col=Conc.Cat, 
+    fill = Conc.Cat)) +
+  scale_fill_brewer(palette="Greens") +  # Part 2: Define the color pallete
+  scale_color_brewer(palette="Greens") + # Part 2: Define the color pallete
+ 
+  geom_dotplot(                          # Part 3: Make the dot plot
                binaxis = "y",         # which axis to bin along
                binwidth = 0.05,       # Minimal difference considered diffeerent
                stackdir = "center",   # Centered
-               dotsize = 0.5
-  )
+               dotsize = 0.5          # Dot size
+              ) + 
+  stat_summary(                         # Part 4: Make a crossbar at the mean
+    fun = mean, 
+    fun.min = mean, 
+    fun.max = mean, 
+    geom = "crossbar",
+    size = 0.3) +                      # Line thickness
+  
+  labs(                                  # Part 4: Change Legend Title
+    fill="Concentration", 
+    color = "Concentration")
 
-
- # geom_dotplot(aes(x=Class,y=DS), method="histodot",stackgroups=TRUE, binwidth = 1)
-
-library(RColorBrewer)
-display.brewer.all()
-
-
-
-p= data %>% 
-  ggplot(aes(x=Class,y=DS)) +
-  geom_point(size=3) +
-  stat_summary(
-    fun.y = mean, fun.ymin = mean, fun.ymax = mean, 
-    geom = "crossbar", 
-    color = "blue",
-    size = 0.3)
-
+### Display the plot
+plot
