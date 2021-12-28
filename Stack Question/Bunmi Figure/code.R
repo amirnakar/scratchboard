f16aa91e2b321f9285a5c0ac2127bc9b01784518
amirnakar <anakar@ymail.com>
2021-12-28 20:20:48 +0100

    Update code.R

--- Stack Question/Bunmi Figure/code.R
+++ Stack Question/Bunmi Figure/code.R
@@ -8,9 +8,9 @@ data = pred_DA_svm
 library(tidyverse)
 
 data$Conc.Cat = as.character(data$Conc)
-
+colnames(data)
 data %>% 
-  ggplot(aes(x=Class,y=DS))+
-  geom_boxplot(aes(fill = Conc.Cat, fill))+
-  geom_jitter(aes(fill = Conc.Cat)) 
+  ggplot(aes(x=Class,y=DS)) +
+  geom_boxplot(aes(fill = Conc)) +
+  geom_jitter(aes(fill = Conc)) 
  # geom_dotplot(aes(x=Class,y=DS), method="histodot",stackgroups=TRUE, binwidth = 1)
